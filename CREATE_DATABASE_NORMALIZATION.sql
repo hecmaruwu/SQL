ceb5cc95d0d079370998fa1994e71636ffd08294
hecmaruwu <64723167+hecmaruwu@users.noreply.github.com>
2026-06-16 00:59:52 -0400

    Update CREATE_DATABASE_NORMALIZATION.sql

--- CREATE_DATABASE_NORMALIZATION.sql
+++ CREATE_DATABASE_NORMALIZATION.sql
@@ -1,1 +1,10 @@
+-- Así se comenta en una sola línea
+/* Así se comenta en
+varias líneas 2 o más*/
 
+--Ejemplo de consulta básica
+SELECT * -- Se seleccionan todas las columnas.
+FROM tabla_principal -- Tabla desde donde se extraen los datos.
+WHERE columna_1 IN ('pan', 'jamón', 'queso'); /* Devuelve solo las filas donde columna_1
+                                                tenga alguno de estos valores:
+                                                'pan', 'jamón' o 'queso'. */
